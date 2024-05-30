3bae6dc002ee52bd4b40f508cc44d512d992444f
Silvio Marcelino de Oliveira <41267790+smarcelino1370@users.noreply.github.com>
2024-05-30 17:42:40 -0300

    build!: reestruturar projeto

--- currency-converter/src/main/java/br/com/currencyconverter/CurrencyConverterApiApplication.java
+++ currency-converter/src/main/java/br/com/currencyconverter/CurrencyConverterApiApplication.java
@@ -1,4 +1,4 @@
-package br.com.soliveira.currencyconverter;
+package br.com.currencyconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- currency-converter/src/test/java/br/com/currencyconverter/CurrencyConverterApiApplicationTests.java
+++ currency-converter/src/test/java/br/com/currencyconverter/CurrencyConverterApiApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.soliveira.currencyconverter;
+package br.com.currencyconverter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
